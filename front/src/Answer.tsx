8b49750904341b467714ed8fb28bd8a5b77b6907
Nikolai Savinkin <nikolai.savinkin@pm.me>
2025-09-09 00:12:51 +0100

    Add types for Question and Answer props

--- front/src/Answer.tsx
+++ front/src/Answer.tsx
@@ -1,3 +1,9 @@
-export function Answer({qid, text, handler}) {
+type AnswerProps = {
+    qid: number;
+    text: string;
+    handler: (id: number, answer: string) => void;
+};
+
+export function Answer({qid, text, handler}: AnswerProps) {
 	return (<button onClick={() => handler(qid, text)}> {text} </button>)
 }
--- front/src/Question.tsx
+++ front/src/Question.tsx
@@ -1,18 +1,32 @@
-import {Answer} from './Answer.tsx'
+import { Answer } from "./Answer.tsx";
 
-export function Question({question, handler}) {
-      return (<>
-		<h1>{question.question}</h1>
-		<ul>
-			{question.answers.map(
-				( answer ) => {
-					return (<li> <Answer
-						qid={question.id}
-						text={answer}
-						handler={handler}
-					/> </li>)
-				}
-			)}
-		</ul>
-	</>)
+type QuestionProps = {
+    question: {
+        id: number;
+        question: string;
+        answers: string[];
+    };
+    handler: (id: number, answer: string) => void;
+};
+
+export function Question({ question, handler }: QuestionProps) {
+    return (
+        <>
+            <h1>{question.question}</h1>
+            <ul>
+                {question.answers.map((answer) => {
+                    return (
+                        <li key={answer}>
+                            {" "}
+                            <Answer
+                                qid={question.id}
+                                text={answer}
+                                handler={handler}
+                            />{" "}
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
+    );
 }
